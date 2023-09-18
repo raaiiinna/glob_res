aa2e341a4348c528bd9a4e92f086c619e34439d5
raaiiinna <78668873+raaiiinna@users.noreply.github.com>
2023-09-17 22:18:27 -0400

    Add files via upload

---
+++ 09_17_23/goals_09_17_23.py
@@ -0,0 +1,20 @@
+'''
+goals for today:
+
+make best plots as possible for each m value
+
+figure out what the end time is for each output chosen, use that as label
+
+try to figure out how to fix mass function, does it give same information?
+
+should I figure out how to give m4.00 on graph? how do i convey this information? as a title?
+
+need massimos help to fix what bins look like for mf
+
+why is MF graph cutting off at 1.5 on x axis?
+
+should I pick values prioritizing even spacing in time or on the graph?
+
+m3.00 goes over bounds on fs plot. should i change bounds of all or just ones that go over? should i use 10^4?
+'''
+
